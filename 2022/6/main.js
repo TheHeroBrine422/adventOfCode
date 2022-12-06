2ed16c47e01ff4a6dacdbf3fa074c12a6bf6988f
Hero <caleb@calebgj.io>
2022-12-05 23:08:19 -0600

    2022 day 6

---
+++ 2022/6/main.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+
+d = fs.readFileSync('data.txt', 'utf8')
+
+t = 0;
+
+lastFourChars = []
+
+for (var i = 0; i < d.length-1; i++) {
+  char = d.charAt(i)
+
+  if (lastFourChars.length == 4) {
+    lastFourChars.splice(0,1)
+  }
+
+  lastFourChars.push(char)
+
+  if (lastFourChars.length == 4) {
+  checkObj = {}
+  fail = false;
+  for (var j = 0; j < lastFourChars.length; j++) {
+    if (checkObj[lastFourChars[j]] == undefined) {
+      checkObj[lastFourChars[j]] = false;
+    } else {
+      fail = true;
+    }
+  }
+
+  if (!fail) {
+    console.log(i+1)
+    console.log(lastFourChars)
+    break
+  }
+}
+}
