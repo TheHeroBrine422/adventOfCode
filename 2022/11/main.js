b786087511f46c258fa355f105a62c4ac2b8d678
Hero <caleb@calebgj.io>
2022-12-11 00:04:39 -0600

    2022 day 11. p2 for this one was very interesting

---
+++ 2022/11/main.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+
+d = fs.readFileSync('data.txt', 'utf8')
+d = d.split("\r\n\r\n")
+
+monkeys = []
+primedDivisbilityModulo = BigInt(1)
+
+for (var i = 0; i < d.length; i++) {
+  d[i] = d[i].split("\r\n")
+  monkeyObj = {
+    items: d[i][1].split(":")[1].split(",").map(x => BigInt(Number(x))),
+    operationType: d[i][2].split(" ")[7] != "old" ? d[i][2].split(" ")[6] : "^",
+    operationNumber: BigInt(isNaN(Number(d[i][2].split(" ")[7])) ? 0 : Number(d[i][2].split(" ")[7])),
+    test: BigInt(Number(d[i][3].split(" ")[5])),
+    trueMonkey: Number(d[i][4].split(" ")[9]),
+    falseMonkey: Number(d[i][5].split(" ")[9]),
+    interactions: 0
+  }
+  primedDivisbilityModulo *= monkeyObj.test
+  monkeys.push(monkeyObj)
+}
+
+for (var i = 0; i < 10000 ; i++) {
+  for (var j = 0; j < monkeys.length; j++) {
+    snapShotLength = monkeys[j].items.length
+    for (var k = 0; k < snapShotLength; k++) {
+      if (monkeys[j].operationType == "*") {
+        monkeys[j].items[0] = (monkeys[j].items[0]*monkeys[j].operationNumber)
+      } else if (monkeys[j].operationType == "+") {
+        monkeys[j].items[0] = (monkeys[j].items[0]+monkeys[j].operationNumber)
+      } else if (monkeys[j].operationType == "^") {
+        monkeys[j].items[0] = (monkeys[j].items[0]*monkeys[j].items[0])
+      }
+
+      monkeys[j].items[0] = monkeys[j].items[0] % primedDivisbilityModulo
+
+      if (monkeys[j].items[0] % monkeys[j].test == 0) {
+        monkeys[monkeys[j].trueMonkey].items.push(monkeys[j].items[0])
+      } else {
+        monkeys[monkeys[j].falseMonkey].items.push(monkeys[j].items[0])
+      }
+
+      monkeys[j].items.shift()
+      monkeys[j].interactions++
+    }
+  }
+}
+
+monkeys.sort(function(a, b){return b.interactions - a.interactions});
+
+console.log(monkeys[0].interactions*monkeys[1].interactions)
